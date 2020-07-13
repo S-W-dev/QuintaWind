0e97b4c6f752f20f01b48982d26781d25ba48045
BuildTools <unconfigured@null.spigotmc.org>
2020-07-12 18:54:25 -0600

    Added Control Panel

---
+++ Pool-Controller/Client/html/ControlPanel/assets/js/index.js
@@ -0,0 +1,45 @@
+let poolTemp = document.getElementById("PoolTemp");
+let spaTemp = document.getElementById("SpaTemp");
+
+let jets = document.getElementById("Jets");
+let waterfalls = document.getElementById("Waterfalls");
+let cleaning = document.getElementById("Cleaning");
+
+let poolTempLabel = document.getElementById("PoolTempLabel");
+let spaTempLabel = document.getElementById("SpaTempLabel");
+
+let jetsLabel = document.getElementById("JetsLabel");
+let waterfallsLabel = document.getElementById("WaterfallsLabel");
+let cleaningLabel = document.getElementById("CleaningLabel");
+
+poolTemp.addEventListener('input', function (evt) {
+    poolTempLabel.innerHTML = this.value;
+});
+
+spaTemp.addEventListener('input', function (evt) {
+    spaTempLabel.innerHTML = this.value;
+});
+
+jets.addEventListener('input', function (evt) {
+    if (this.checked) {
+        jetsLabel.innerHTML = "Enabled";
+    } else {
+        jetsLabel.innerHTML = "Disabled";
+    }
+});
+
+waterfalls.addEventListener('input', function (evt) {
+    if (this.checked) {
+        waterfallsLabel.innerHTML = "Enabled";
+    } else {
+        waterfallsLabel.innerHTML = "Disabled";
+    }
+});
+
+cleaning.addEventListener('input', function (evt) {
+    if (this.checked) {
+        cleaningLabel.innerHTML = "Enabled";
+    } else {
+        cleaningLabel.innerHTML = "Disabled";
+    }
+});
