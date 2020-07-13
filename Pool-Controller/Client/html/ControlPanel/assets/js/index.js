c9c6e8d73ba5beb15b309725fe9e86c314036618
BuildTools <unconfigured@null.spigotmc.org>
2020-07-13 11:27:19 -0600

    iugkug

--- Pool-Controller/Client/html/ControlPanel/assets/js/index.js
+++ Pool-Controller/Client/html/ControlPanel/assets/js/index.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3001');
+const socket = io('http://localhost:3000');
 
 let poolTemp = document.getElementById("PoolTemp");
 let spaTemp = document.getElementById("SpaTemp");
